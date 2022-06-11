0ace4883aaa158ee48b655d467c93ebd0a6fb478
molefe <molefe@mmwebdesign.co.za>
2022-06-11 08:59:13 +0200

    started working on portfolio section added images place holder , title and filter titles

--- src/components/portfolio/Portfolio.component.jsx
+++ src/components/portfolio/Portfolio.component.jsx
@@ -5,13 +5,35 @@ const Portfolio = () => {
 		<div className="portfolio" id="portfolio">
 			<h1>Portfolio</h1>
 			<ul>
-				<li>All</li>
+				<li className="active">All</li>
 				<li>Custom</li>
 				<li>Wordpress</li>
 			</ul>
 			<div className="container">
-				<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
-				<h3>Brain Space</h3>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/screen.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
+				<div className="item">
+					<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+					<h3>Brain Space</h3>
+				</div>
 			</div>
 		</div>
 	);
