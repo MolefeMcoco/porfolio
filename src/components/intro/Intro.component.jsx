0a9b53c251078c8b3ca5b3fb257285fa866a33d7
molefe <molefe@mmwebdesign.co.za>
2022-06-11 07:53:45 +0200

    i added an image with a background on the header and titles with my name and what i do

--- src/components/intro/Intro.component.jsx
+++ src/components/intro/Intro.component.jsx
@@ -1,9 +1,24 @@
 import './intro.syles.scss';
+import downArrow from '../../assets/down-arrow.png';
 
 const Intro = () => {
 	return (
 		<div className="intro" id="intro">
-			Intro
+			<div className="left">
+				<div className="imageContainer" />
+			</div>
+			<div className="right">
+				<div className="wrapper">
+					<h2>Hi There, I'm </h2>
+					<h1>Molefe Mcoco</h1>
+					<h3>
+						Freelance <span> Designer</span>
+					</h3>
+				</div>
+				<a href="#portfolio">
+					<img src={downArrow} alt="" />
+				</a>
+			</div>
 		</div>
 	);
 };
--- src/components/portfolio/Portfolio.component.jsx
+++ src/components/portfolio/Portfolio.component.jsx
@@ -3,7 +3,16 @@ import './portfolio.styles.scss';
 const Portfolio = () => {
 	return (
 		<div className="portfolio" id="portfolio">
-			Portfolio
+			<h1>Portfolio</h1>
+			<ul>
+				<li>All</li>
+				<li>Custom</li>
+				<li>Wordpress</li>
+			</ul>
+			<div className="container">
+				<img src="https://mmwebdesign.co.za/assets/images/1.jpg" alt="" />
+				<h3>Brain Space</h3>
+			</div>
 		</div>
 	);
 };
